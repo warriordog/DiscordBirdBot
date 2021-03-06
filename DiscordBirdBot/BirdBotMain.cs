52bfdec84a91f12196dbbede8d49518c05bca4e9
Chris Koehler <acomputerdog@gmail.com>
2021-03-05 22:33:01 -0500

    Log when bot joins a server

--- DiscordBirdBot/BirdBotMain.cs
+++ DiscordBirdBot/BirdBotMain.cs
@@ -30,12 +30,19 @@ namespace DiscordBirdBot
                     Token = _options.DiscordToken,
                     TokenType = TokenType.Bot,
                     LoggerFactory = loggerFactory,
-                    Intents = DiscordIntents.DirectMessages | DiscordIntents.GuildMessages
+                    Intents = DiscordIntents.DirectMessages | DiscordIntents.GuildMessages | DiscordIntents.Guilds
                 }
             );
 
             // Register event handler
             _discord.MessageCreated += HandleMessage;
+            
+            // Log when bot joins a server.
+            _discord.GuildCreated += (d, e) =>
+            {
+                _logger.LogInformation($"Joined server {e.Guild.Id} ({e.Guild.Name})");
+                return Task.CompletedTask;
+            };
         }
 
         private async Task HandleMessage(DiscordClient sender, MessageCreateEventArgs e)
